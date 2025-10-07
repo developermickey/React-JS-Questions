d17df1e74932b9b92a97a337134f85ccce9e138c
developermickey <mukesh.quazma@gmail.com>
2025-10-07 07:14:55 +0530

    Q1. React_lecture13_counter

---
+++ Day 193/q1/src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state here
+    this.state = {
+      count: 0,
+      showCount: true,
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrementCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <div>Count: {this.state.count}</div>
+        <button onClick={this.incrementCount}>Increment</button>
+        <button onClick={this.decrementCount}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
